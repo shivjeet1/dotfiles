37dac569fce59a4f8396f6349897f2acccfa355d
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-02-15 11:23:06 +0530

    deleted

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cee6e6", "#030305" },
-	[SchemeSel] = { "#cee6e6", "#B58571" },
-	[SchemeOut] = { "#cee6e6", "#92B0D3" },
-};
