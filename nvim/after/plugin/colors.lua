87ca8e6006dad7c1acc43bad3cf85eb3468dc800
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-03-18 10:04:31 +0530

    kjfjdhajh

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,9 @@
+function ColormyChars(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+	
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normalfloat", { bg = "none" })
+end
+
+ColormyChars()
