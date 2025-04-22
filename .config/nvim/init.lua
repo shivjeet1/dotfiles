555442c1cef2c814b47e0097f244b80e00ee7b55
shivjeet1 <shivamlavhalepatil@gmail.com>
2025-04-22 19:05:50 +0530

    asdf;lkjh

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,17 @@
-require("shivjeet1")
-
---print("hellofromhome")
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.lualine"
+require "user.alpha"
+require "user.toggleterm"
+require "user.indentline"
