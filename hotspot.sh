6ca8621a5e8d8638611c32c365850b1501086f43
shivjeet1 <shivamlavhalepatil@gmail.com>
2026-07-16 15:53:30 +0530

    changed a bit

--- hotspot.sh
+++ hotspot.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-sudo iptables -t nat -A POSTROUTING -o enp0s31f6 -j MASQUERADE
-sudo iptables -A FORWARD -i wlp2s0 -o enp0s31f6 -j ACCEPT\
-sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-sudo nmcli con down shivam
-nmcli con down shivam
-sudo nmcli device wifi hotspot ifname wlp2s0 ssid shivam password shivam2285k
+iptables -t nat -A POSTROUTING -o enp0s31f6 -j MASQUERADE
+iptables -A FORWARD -i wlp2s0 -o enp0s31f6 -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+nmcli con down arch
+nmcli device wifi hotspot ifname wlp2s0 ssid arch password shivam2285k
 
