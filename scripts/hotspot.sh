134fe396aa2fa51b3b692e79d9489e17af5b795a
shivjeet1 <shivamlavhalepatil@gmail.com>
2026-07-27 11:52:04 +0530

    switched somethin

--- scripts/hotspot.sh
+++ scripts/hotspot.sh
@@ -3,6 +3,6 @@
 iptables -t nat -A POSTROUTING -o enp0s31f6 -j MASQUERADE
 iptables -A FORWARD -i wlp2s0 -o enp0s31f6 -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-nmcli con down arch
+nmcli con reload arch
 nmcli device wifi hotspot ifname wlp2s0 ssid arch password shivam2285k
 
